a55b5fc964cb8501ee62941d8391551115251f5b
Parid Upapong <paris.u2528@gmail.com>
2026-01-22 23:27:00 +0700

    SWARM_SYNC: Update terraform/s3.tf

---
+++ terraform/s3.tf
@@ -0,0 +1,42 @@
+resource "aws_s3_bucket" "assets" {
+  bucket = var.asset_bucket_name
+}
+
+resource "aws_s3_bucket_public_access_block" "assets_block" {
+  bucket = aws_s3_bucket.assets.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "assets_lifecycle" {
+  bucket = aws_s3_bucket.assets.id
+
+  rule {
+    id     = "archive-old-versions"
+    status = "Enabled"
+
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "GLACIER"
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "assets_cors" {
+  bucket = aws_s3_bucket.assets.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "POST"]
+    allowed_origins = ["https://app.archive-ai.com"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
